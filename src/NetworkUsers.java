e3ed47b19351068d830a835e70869d292f322032
Collin Hargreaves <collinj@Collins-MacBook-Air.local>
2023-05-27 22:43:52 -0700

    push

--- src/NetworkUsers.java
+++ src/NetworkUsers.java
@@ -1,3 +1,5 @@
+
+
 public class NetworkUsers {
     
     
@@ -7,33 +9,44 @@ public class NetworkUsers {
         profiles = new HashedDictionary<>();
     }
 
-    public void joinNetwork(Profile profile) {
+    public void joinNetwork(Profile profile) throws UserAlreadyExistsException {
         if (profiles.contains(profile.getName().toLowerCase())) {
-            
+            throw new UserAlreadyExistsException();
         }
+
         profiles.add(profile.getName().toLowerCase(), profile);
     }
 
-    public void leaveNetwork(Profile profile) {
-        profiles.remove(profile.getName());
+    public void leaveNetwork(Profile profile) throws NoSuchUserException {
+        if (profiles.getValue(profile.getName().toLowerCase()) == null) {
+            throw new NoSuchUserException();
+        } else {
+            profiles.remove(profile.getName().toLowerCase());
+        }
     }
 
     // this will essentially change the name of the profile and the key
     // this allows for an edit in the username to make sure the key is also changed
     // so it still corresponds to the expected profile
 
-    public void modifyProfile(Profile profile, String newName) {
-        if (profile.getName() != null) {
+    public void modifyProfile(Profile profile, String newName) throws UserAlreadyExistsException {
+        if (profile.getName().toLowerCase() != null) {
+            newName = newName.toLowerCase();
             profile.setName(newName);
         }
+
+
     }
 
-    public Profile searchProfile(String userName) {
+    public Profile searchProfile(String userName) throws NoSuchUserException {
+        userName = userName.toLowerCase();
+        if (profiles.getValue(userName) == null) {
+            throw new NoSuchUserException();
+        }
         return profiles.getValue(userName);
-
     }
 
-    // creating a subclas of class exception
+    // creating a subclass of class exception
     // two constructors
     // first one is default, gives error message
     // second one can be a given String message
@@ -46,6 +59,18 @@ public class NetworkUsers {
             super(message);
         }
 
+    }
+
+    // subclass of class exception
+    // same thing as above except for the case where there is no user with the key
+    public class NoSuchUserException extends Exception {
+        public NoSuchUserException() {
+            super("User does not exist..");
+        }
+
+        public NoSuchUserException(String message) {
+            super(message);
+        }
     }
 }
     
